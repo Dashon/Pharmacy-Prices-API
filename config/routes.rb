85feda8c3984f2e171794e173c2c7a366f043bec
Dashon Howard <dhoward@pathf.com>
2016-04-22 22:56:39 -0500

    use api route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     {}
   end
 
-  namespace :api, path: '', constraints: api_constraints, defaults: {format: :json} do
+  namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :users
       resources :drug_prices
