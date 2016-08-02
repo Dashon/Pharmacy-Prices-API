6897755e241faeb2085ae0dd60bc17205f0b8792
Dashon Howard <dhoward@pathf.com>
2016-08-02 16:47:54 -0500

    disable pagination

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -42,27 +42,27 @@ class Api::ApiController < ActionController::Base
 
 
   def set_pagination_header(name, options = {})
-    scope = instance_variable_get("@#{name}")
-    if !scope.nil?
-      request_params = request.query_parameters
-      url_without_params = request.original_url.slice(0..(request.original_url.index("?")-1)) unless request_params.empty?
-      url_without_params ||= request.original_url
+    # scope = instance_variable_get("@#{name}")
+    # if !scope.nil?
+    #   request_params = request.query_parameters
+    #   url_without_params = request.original_url.slice(0..(request.original_url.index("?")-1)) unless request_params.empty?
+    #   url_without_params ||= request.original_url
 
-      page = {}
-      page[:first] = 1 if scope.total_pages > 1 && !scope.first_page?
-      page[:last] = scope.total_pages  if scope.total_pages > 1 && !scope.last_page?
-      page[:next] = scope.current_page + 1 unless scope.last_page?
-      page[:prev] = scope.current_page - 1 unless scope.first_page?
+    #   page = {}
+    #   page[:first] = 1 if scope.total_pages > 1 && !scope.first_page?
+    #   page[:last] = scope.total_pages  if scope.total_pages > 1 && !scope.last_page?
+    #   page[:next] = scope.current_page + 1 unless scope.last_page?
+    #   page[:prev] = scope.current_page - 1 unless scope.first_page?
 
-      pagination_links = []
-      page.each do |k, v|
-        new_request_hash= request_params.merge({:page => v})
-        pagination_links << "<#{url_without_params}?#{new_request_hash.to_param}>; rel=\"#{k}\""
-      end
-      headers["Link"] = pagination_links.join(", ")
-      headers["Total_pages"] = scope.total_pages
-      headers["Current_page"] = scope.current_page
-    end
+    #   pagination_links = []
+    #   page.each do |k, v|
+    #     new_request_hash= request_params.merge({:page => v})
+    #     pagination_links << "<#{url_without_params}?#{new_request_hash.to_param}>; rel=\"#{k}\""
+    #   end
+    #   headers["Link"] = pagination_links.join(", ")
+    #   headers["Total_pages"] = scope.total_pages
+    #   headers["Current_page"] = scope.current_page
+    # end
   end
 
 
