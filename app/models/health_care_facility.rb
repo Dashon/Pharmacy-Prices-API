115303fd052058625bbfffeb2f1c57d14ee2e7b9
Dashon Howard <dhoward@pathf.com>
2016-08-03 05:36:48 -0500

    disable paperclip for cf's

--- app/models/health_care_facility.rb
+++ app/models/health_care_facility.rb
@@ -9,15 +9,15 @@ class HealthCareFacility < ActiveRecord::Base
   has_many :contracted, :through => :contracted_pharmacies , source: "dni_pharmacy"
   has_many :rewards, :through => :hcf_rewards
 
-  validates :image_url,
-    attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
-    attachment_size: { less_than: 2.megabytes }
+  # validates :image_url,
+  #   attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
+  #   attachment_size: { less_than: 2.megabytes }
 
-  has_attached_file :image_url, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  },  default_url:'/assets/images/missing/default_:style.png'
+  # has_attached_file :image_url, styles: {
+  #   thumb: '100x100>',
+  #   square: '200x200#',
+  #   medium: '300x300>'
+  # },  default_url:'/assets/images/missing/default_:style.png'
 
   def active_model_serializer
     ShortHealthCareFacilitySerializer
