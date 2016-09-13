8b3fb1b7eda9e292cde3155fe980fe6cd7008d82
Dashon Howard <i@dashon.co>
2016-09-13 09:51:58 -0500

    remove invatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   # Include default devise modules.
-  devise :invitable, :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable,
     :confirmable
   belongs_to :health_care_facility
