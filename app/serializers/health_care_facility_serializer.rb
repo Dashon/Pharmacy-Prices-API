865da3de359205a4c675aef44b4d97dbb61ad4a7
Dashon Howard <i@dashon.co>
2016-11-06 13:19:56 -0600

    remove users

--- app/serializers/health_care_facility_serializer.rb
+++ app/serializers/health_care_facility_serializer.rb
@@ -1,6 +1,6 @@
 class HealthCareFacilitySerializer < ActiveModel::Serializer
   attributes :id, :name, :image_url, :team_goal,:month_trophy,:next_reward
-  has_many :users
+  #has_many :users
   has_many :hcf_locations
   has_many :rewards
 end
