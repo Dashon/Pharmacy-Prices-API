30621ba669ffcb8db6a1f23d038183a3b3f3fb85
Dashon Howard <i@dashon.co>
2016-11-07 02:02:32 -0600

    remove hcf from user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :password, :api_token, :api_rpm, :name, :role, :invited_by_id, :health_care_facility_id, :month_points, :total_points, :total_points, :trophies, :avatars, :badges, :image_url, :survey_day, :todays_surveys
-  has_one :health_care_facility
   has_one :role
   has_many :rewards
 end
