da1208649a7b9719fb9ed6b2aef070e61e86d4b1
Dashon Howard <i@dashon.co>
2016-11-07 01:51:20 -0600

    move :team_total_points, :team_month_points

--- app/serializers/health_care_facility_serializer.rb
+++ app/serializers/health_care_facility_serializer.rb
@@ -1,5 +1,5 @@
 class HealthCareFacilitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :image_url, :team_goal,:top_users,:month_trophy,:next_reward
+  attributes :id, :name, :image_url, :team_goal,:top_users,:month_trophy,:next_reward,:team_total_points, :team_month_points
   has_many :users
   has_many :hcf_locations
   has_many :rewards
