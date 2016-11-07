ae92db57c80abd4812afffd83922b0c453fe4f74
Dashon Howard <i@dashon.co>
2016-11-07 09:17:36 -0600

    use all_users

--- app/serializers/health_care_facility_serializer.rb
+++ app/serializers/health_care_facility_serializer.rb
@@ -1,5 +1,5 @@
 class HealthCareFacilitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :image_url, :team_goal,:top_users,:month_trophy,:next_reward,:team_total_points, :team_month_points
+  attributes :id, :name, :image_url, :team_goal,:top_users,:month_trophy,:next_reward,:team_total_points, :team_month_points, :all_users
   # has_many :users
   has_many :hcf_locations
   has_many :rewards
